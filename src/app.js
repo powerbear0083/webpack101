e6cc6ecef0b88bdc64ff93a1560117f8b6cdf5d4
eu-company <powerbear008@gmail.com>
2017-05-05 18:29:06 +0800

    film 12 practice

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
-const css = require('./app.sass');
+// const css = require('./app.sass');
 
-console.log('Hellow from app.js dsfdasdasdsfasdasdasdasdasd')
+// console.log('Hellow from app.js and webpack dev server!!!!')
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(
+//   <h1>Hello, world!!!!!@@@@</h1>,
+//   document.getElementById('boot')
+// );
+
+import css from './app.sass';
+
+console.log('testing hot module replacement!!!!!!')
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,64 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack = require('webpack');
+var path = require("path");
+var bootstrapEntryPoints = require('./webpack.bootstrap.config.js'); 
+
+var isProd = process.env.NODE_ENV === 'production'; // true or false
+var cssDev = ['style-loader','css-loader', 'sass-loader'];
+var cssProd = ExtractTextPlugin.extract({
+	fallback: 'style-loader',
+	loader: ['css-loader','sass-loader'],
+	publicPath: '/dist'
+});
+var cssConfig = isProd ? cssProd : cssDev;
+
+var bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
 
 module.exports = {
-	entry: './src/app.js',
+	entry: {
+		app: './src/app.js',
+		contact: './src/contact.js',
+		bootstrap: bootstrapConfig
+	},
 	output: {
-		path: '/Users/eu-team/eu-work/self-practice/08_webpack101/dist',
-		filename: 'app.bundle.js'
+		// path: '/Users/eu-team/eu-work/self-practice/08_webpack101/dist',
+		path: path.resolve(__dirname, "dist"),
+		filename: '[name].bundle.js'
 	},
 	module: {
 		rules: [
 			{
 				test: /\.(sass|scss)$/, 
-				use: ExtractTextPlugin.extract({
-		                fallback: 'style-loader',
-		                loader: ['css-loader', 'sass-loader'],
-		                publicPath: '/dist'
-					}
-				)
+				use: cssConfig
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.pug$/,
+        		use: ['html-loader', 'pug-html-loader']
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+        		use: [
+        			// 'file-loader?name=[name].[hash:6].[ext]&outputPath=img/&publicPath=',
+        			'file-loader?name=img/[name].[hash:6].[ext]',
+        			'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        		]
 			}
 		]
 	},
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        hot: true,
+        port: 9000,
+        stats: 'errors-only',
+        open: true
+    },
 	plugins: [
 		new HtmlWebpackPlugin({
 		  title: 'Project Demo',
@@ -27,12 +66,25 @@ module.exports = {
 		  // 	collapseWhitespace: true
 		  // },
 		  hash: true,
-		  template: './src/index.html'
+		  excludeChunks: ['contact'],
+		  template: './src/index.pug'
+		}),
+		new HtmlWebpackPlugin({
+		  title: 'Contact Page',
+		  // minify: {
+		  // 	collapseWhitespace: true
+		  // },
+		  hash: true,
+		  chunks: ['contact'],
+		  filename: 'contact.html',
+		  template: './src/contact.html'
 		}),
 		new ExtractTextPlugin({
 			filename: 'app.css',
-			disable: false,
+			disable: !isProd,
 			allChunks: true
-		})
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NamedModulesPlugin()
 	]
 }
